b0151f020729af16e5c5b0c53201929d49129e5a
skyloungecafe4-beep <skyloungecafe4@gmail.com>
2026-08-07 13:51:46 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -189,4 +189,8 @@ def delete_item(item_id):
 
 
 if __name__ == "__main__":
-  app.run(debug=True, port=5000)
+  import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
